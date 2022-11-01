8c6b95240c569f6737a5d3df5f62d024ee554a3d
Kassandra <K1995Moreno@gmail.com>
2022-11-01 17:00:05 -0400

    commit tree 1b0fe25f8fc00bd5cb8f54318f30ae6c77858598

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+let employee={
+    name: "Kasey",
+    address: "000 Rock Bottom",
+}
+
+//Nondestructive updateEmployeeWithKeyAndValue
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {...employee,...{[key]:value}}
+}
+
+//Destructive updateEmployeeWithKey
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key]=value
+    return employee
+}
+
+//Nondestructive deleteFromEmployeeByKey
+let employeeCopy= {}
+function deleteFromEmployeeByKey(employee, key){
+    employeeCopy= {...employee}
+    delete employeeCopy[key]
+    return employeeCopy
+}
+
+//Destructive deleteFromEmployeeByKey
+function destructivelyDeleteFromEmployeeByKey(employee, name){
+    delete employee[name]
+    return employee
+}
+
